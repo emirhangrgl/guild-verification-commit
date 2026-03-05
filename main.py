3f3992e26d4a8bc3b2bdfa2be6b542382cb42f4f
emirhangrgl <emirhangor264@gmail.com>
2026-03-05 14:11:26 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:11:20.435Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T11:11:25.504Z
+Commit: Resolve null pointer exception in user service
