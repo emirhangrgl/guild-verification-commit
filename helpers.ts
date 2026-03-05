7115df1531d86b6a85d0b1bb38d68c293133fda2
emirhangrgl <emirhangor264@gmail.com>
2026-03-05 13:49:57 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T10:49:54.747Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T10:49:57.151Z
+Commit: Patch security vulnerability in authentication
