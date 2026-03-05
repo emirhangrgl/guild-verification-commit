41625b6844e419618769d23871ede81d1f0f7896
emirhangrgl <emirhangor264@gmail.com>
2026-03-05 13:57:12 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T10:49:57.151Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T10:57:12.074Z
+Commit: Add multi-language support
