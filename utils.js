ff61fba6c65f43e8129db8c5834280dde20e140f
emirhangrgl <emirhangor264@gmail.com>
2026-03-05 13:50:23 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T10:50:02.451Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T10:50:22.787Z
+Commit: Fix race condition in async operations
