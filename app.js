40faef967d5e92cdbc6103c32763145d380b01ac
emirhangrgl <emirhangor264@gmail.com>
2026-03-05 14:11:30 +0300

    Add export functionality for reports

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:11:11.717Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T11:11:30.466Z
+Commit: Add export functionality for reports
